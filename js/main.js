f4d22dc3f151590e6d57c6f10bb4c8edbcfeddd8
Niket Shukla <47295160+niketshukla@users.noreply.github.com>
2021-12-16 10:57:35 -0500

    If applied will change main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
-let pokemonList = [
-    {
-        name: 'pikachu',
-        height: 0.4,
-        type: ['electric']
-    },
-    {
-        name: 'charmander',
-        height: 0.6,
-        type: ['fire']
-    },
-    {
-        name: 'bulbasaur',
-        height: 0.7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'pidgeot',
-        height: 1.5,
-        type: ['flying', 'normal']
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'pikachu',
+            height: 0.4,
+            type: ['electric']
+        },
+        {
+            name: 'charmander',
+            height: 0.6,
+            type: ['fire']
+        },
+        {
+            name: 'bulbasaur',
+            height: 0.7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'pidgeot',
+            height: 1.5,
+            type: ['flying', 'normal']
+        }
+    ];
+  
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
 
-pokemonList.forEach (function (poke) {
+
+  pokemonRepository.getAll().forEach (function (poke) {
     if(poke.height > 1 && poke.height < 1.6){
         document.write(`<p> ${poke.name} (height: ${poke.height}) - Wow that's so big </p>`)
     } else {
